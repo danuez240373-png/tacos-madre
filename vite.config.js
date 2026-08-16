476c4c6bc1b6865d359f4fd254f297ce7e1042f6
Daniel Nunez <danuez240373@gmail.com>
2026-08-16 12:51:22 -0700

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/tacos-madre/',
   plugins: [react()],
   server: { port: 5173, open: false },
   build: { outDir: 'dist', assetsInlineLimit: 2048 },
