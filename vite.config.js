5e7de2ca2adca44f96583210dd9eb6bb97c0f406
Daniel Nunez <danuez240373@gmail.com>
2026-08-16 15:28:57 -0700

    Make deployment paths universal

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tacos-madre/',
+  base: './',
   plugins: [react()],
   server: { port: 5173, open: false },
   build: { outDir: 'dist', assetsInlineLimit: 2048 },
